8fff4ffd63f874c711a42e412c52b9cad0f04fe6
galibmia <gm71bd@gmail.com>
2024-10-27 11:01:35 +0600

    Design all contact page with sample data

--- src/pages/AllContacts.jsx
+++ src/pages/AllContacts.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { TbUserEdit } from "react-icons/tb";
+import { MdDeleteForever } from "react-icons/md";
+
+
 
 function AllContacts() {
     const [contacts, setContacts] = useState(null);
@@ -17,10 +21,10 @@ function AllContacts() {
 
   return (
     <div className='w-4/5 mx-auto'>
-        <h1>This is all contact page</h1>
-        <div className='grid grid-cols-3 gap-8'>
+        <h1 className='text-4xl font-bold text-center mt-14 mb-8'>Your contacts</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8'>
             {
-                contacts.map(contact =>  <div key={contact.id} className="card w-96 bg-base-100 shadow-xl">
+                contacts.map(contact =>  <div key={contact.id} className="card w-full md:w-96 xl:w-full 2xl:w-96 mx-auto bg-base-100 shadow-xl">
                     <figure>
                       <img src={contact.profilePicture} alt={`${contact.name}'s profile`} className="object-cover h-80 w-full" />
                     </figure>
@@ -30,11 +34,11 @@ function AllContacts() {
                       <p>Phone: {contact.phone}</p>
                       <p>Address: {contact.address}</p>
                       <div className="card-actions justify-end">
-                        <button className="btn btn-primary" onClick={() => onUpdate(contact.id)}>
-                          Update
+                        <button className="btn bg-violet-400" onClick={() => onUpdate(contact.id)}>
+                         <TbUserEdit className='text-xl'/> Update
                         </button>
-                        <button className="btn btn-error" onClick={() => onDelete(contact.id)}>
-                          Delete
+                        <button className="btn text-white btn-error" onClick={() => onDelete(contact.id)}>
+                         <MdDeleteForever className='text-xl'/> Delete
                         </button>
                       </div>
                     </div>
